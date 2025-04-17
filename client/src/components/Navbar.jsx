a17452d4592fdf2b044fff5207337984030718a3
alok7139 <alokgarg7139@gmail.com>
2025-04-17 21:38:10 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
+    const [visible, setvisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.shopeasy} alt='logo' className='w-38'/>
@@ -53,6 +55,23 @@ function Navbar() {
                <img src={assets.cart_icon} className='w-5 min-w-5'/>
                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 text-white bg-black aspect-square rounded-full text-[8px]' >10</p>
             </Link>
+            <img onClick={() => setvisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden'/>
+        </div>
+
+        {/* sidebar menu for smaller screen */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+             <div className='flex flex-col text-gray-600'>
+                <div onClick={() => setvisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img src={assets.dropdown_icon} className='h-4 rotate-180' />
+                    <p>Back</p>
+                </div>
+
+                <NavLink onClick={() => setvisible(false)} className={`py-2 pl-6 border`} to={'/'}>HOME</NavLink>
+                <NavLink onClick={() => setvisible(false)} className={`py-2 pl-6 border`} to={'/collection'}>COLLECTION</NavLink>
+                <NavLink onClick={() => setvisible(false)} className={`py-2 pl-6 border`} to={'/about'}>ABOUT</NavLink>
+                <NavLink onClick={() => setvisible(false)} className={`py-2 pl-6 border`} to={'/contact'}>CONTACT</NavLink>
+
+             </div>
         </div>
       
     </div>
