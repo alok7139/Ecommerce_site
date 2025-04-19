563d592a64828e64451db213336809f8c1ecf0c6
alok7139 <alokgarg7139@gmail.com>
2025-04-19 18:53:51 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -5,4 +5,23 @@ import { config } from 'dotenv'
 
 // app config
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+       origin: [process.env.FRONTEND_URI],
+       methods: ["GET" , "POST" , "PUT" , "DELETE"],
+       credentials:true,
+}))
+
+// api endpoint
+app.get('/' , (req,res) => {
+    res.send('API WORKING');
+})
+
+app.listen(port , () => {
+    console.log(`server is listen on ${port}`)
+})
+
